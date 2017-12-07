daf1f2996b73a8b72c99ac0ead86006a38addbf3
Christian Roring <c.roring@recognize.nl>
2017-12-07 20:41:41 +0100

    Added code to store the last state of the switch into memory

--- relayonpress.c
+++ relayonpress.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   relayonpress.c
  * Author: Benoit
  *
@@ -24,20 +24,26 @@ void main(void) {
    TRISIO3 = 1; // input temporary switch
 
    GPIO = 0; // set outputs as low level (0V)
-   
+
    // Variables definition
    uint8_t state; // on-off state of the pedal (1 = on, 0 = off)
    state = 0;
-   
-   uint8_t changestate; // to change status of the pedal
-   changestate = 0;
-   
+
    uint8_t temporary; // define the mode of the pedal : classic of temporary activation
    temporary = 0;
-   
+
    uint8_t press_switch; // variable to detect if the switch is pressed
    press_switch = 0;
-   
+
+   // uint8_t debug_value;
+   // debug_value = 0x00;
+
+   uint8_t change_state; // to change status of the pedal
+    if( eeprom_read(0) == 0xFF )	// Read from 0 address location
+        change_state = 1;			// If previous state was on
+    else
+        change_state = 0;			// If previous state was off
+
    // Main loop
    while(1) {
        if(GP3 == 0) { // By default: normal mode
@@ -46,56 +52,56 @@ void main(void) {
       else { // If the "hold" mode is activated through the switch
           temporary = 1;
       }
-       
+
        // Normal mode
-       if(temporary == 0) { 
+       if(temporary == 0) {
            if(GP1 == 0) { // footswitch pressed
                __delay_ms(15); // debouncing
-               if(GP1 == 0) { 
+               if(GP1 == 0) {
                   press_switch = press_switch + 1; // switch is on
                   if(press_switch>10) {
                       press_switch = 10; // max value for press_switch
-                  } 
+                  }
                }
            }
            if(press_switch == 1) { // switch is pressed : lets turn the pedal on or off
-               changestate = 1;
+               change_state = 1;
                press_switch = 2; // avoid bugs if press_switch stays at 1
            }
            if(GP1 == 1) {
                __delay_ms(15); // debouncing
-               if(GP1 == 1) { 
-                  press_switch = 0; 
+               if(GP1 == 1) {
+                  press_switch = 0;
                }
            }
        }
-       
+
        // Temporary mode
        if(temporary == 1) {
            if(GP1 == 0) { // if switch is pressed : temporary activation
                __delay_ms(15); // debouncing
-               if(GP1 == 0) { 
+               if(GP1 == 0) {
                   press_switch = press_switch + 1; // switch is on
                   if(press_switch>10) {
                       press_switch = 10; // max value for press_switch
-                  } 
+                  }
                }
            }
            if(GP1 == 1) { // if switch is not pressed, turn it off
                __delay_ms(15); // debouncing
-               if(GP1 == 1) { 
+               if(GP1 == 1) {
                    state = 0;
                    press_switch = 0;
                }
            }
            if(press_switch == 1) {
-               changestate = 1; // if switch is pressed, turn the pedal on
+               change_state = 1; // if switch is pressed, turn the pedal on
                press_switch = 2; // avoids bug
            }
        }
-       
+
        // Changing state of the pedal
-       if(changestate == 1) {
+       if(change_state == 1) {
            if(state == 0) { // change to on
                GP2 = 1; // photoFET on
                __delay_ms(20);
@@ -105,6 +111,7 @@ void main(void) {
                __delay_ms(20);
                GP2 = 0; // photoFET off
                state = 1;
+               eeprom_write(0, 0xFF);		// Write 0xFF at 0 address location
            }
           else { // change to off
                GP2 = 1;
@@ -115,21 +122,24 @@ void main(void) {
                __delay_ms(20);
                GP2 = 0;
                state = 0;
+               eeprom_write(0, 0x00);		// Write 0x00 at 0 address location
            }
           __delay_ms(10);
-          changestate=0; // reset changestate
+          change_state=0; // reset change_state
        }
-       
+
+       // debug_value = eeprom_read(0);
+
        // To let the pedal in the good state (on or off)
        if (state == 1) { // effect on
             GP0 = 1; // LED on
             GP5 = 1; // relay on
-            GP4 = 0; 
+            GP4 = 0;
        }
         else { // effect off
             GP0 = 0; // LED off
             GP5 = 0; // relay off
-            GP4 = 0; 
+            GP4 = 0;
        }
    }
 }
