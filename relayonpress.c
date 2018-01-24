f57a32f133166d7261d52d8e151ca3cb5a7fcb69
Christian Roring <c.roring@recognize.nl>
2018-01-24 22:50:18 +0100

    Cleanup

--- relayonpress.c
+++ relayonpress.c
@@ -1,8 +1,10 @@
 /*
  * File:   relayonpress.c
- * Author: Benoit
+ * Author: Christian Roring
  *
- * Created on 5/02/2017
+ * Created on 24/01/2018
+ * 
+ * Based on code by: Benoit
  */
 
 #include <stdio.h>
@@ -11,11 +13,11 @@
 #include <xc.h>
 #include "header.h"
 
-volatile uint32_t tick_count;
+volatile uint32_t tick_count; // Make it available for use in interrupt
 
 void InitTimer0(void) {
     // Timer0 is a 8bit timer, select T0CS and PSA to be one
-    OPTION_REG &= 0x83; //Make Prescaler 1:16 (1 MIPS: 256 (8bit) *16) +/- 0.004 sec
+    OPTION_REG &= 0x83; // Make Prescaler 1:16 (1 MIPS: 256 (8bit) * 16) +/- 0.004 sec
 
     T0IE = 1; // Enable Timer0 interrupt
     GIE = 1; // Enable global interrupts
@@ -35,12 +37,12 @@ void main(void) {
     ANSEL = 0; // no analog GPIOs
     CMCON = 0x07; // comparator off
     ADCON0 = 0; // Analog to Digital and Digital to Analog convertors off
-    TRISIO0 = 0; // output LED
-    TRISIO1 = 1; // input footswtich
-    TRISIO2 = 0; // output TGP222A
-    TRISIO5 = 0; // output activation relay
-    TRISIO4 = 0; // output ground relay
-    TRISIO3 = 1; // input temporary switch
+    TRISIO0 = 0; // output LED -> GP0
+    TRISIO1 = 1; // input footswtich -> GP1
+    TRISIO2 = 0; // output TGP222A -> GP2
+    TRISIO5 = 0; // output activation relay -> GP3
+    TRISIO4 = 0; // output ground relay -> GP4
+    TRISIO3 = 1; // input temporary switch -> GP5
 
     GPIO = 0; // set outputs as low level (0V)
 
@@ -48,7 +50,7 @@ void main(void) {
     tick_count = 0; // tick count for the interrupt timer
 
     uint8_t state = 0; // on-off state of the pedal (1 = on, 0 = off)
-    uint8_t button_state = 1; // debounce state for the button
+    uint8_t button_state = 1; // Debounce state for the button
     uint8_t hold_mode = 0; // State for the hold mode indicator
     
     uint8_t change_state; // to change status of the pedal
@@ -64,15 +66,14 @@ void main(void) {
     while (1) {
         // If the switch is pressed
         if (GP1 == 0) {
-            // debouncing
-            __delay_ms(10);
+            __delay_ms(10); // Debouncing
             if (GP1 == 0) {
                 if (GP1 != button_state) {
                     button_state = GP1;
                     change_state = 1;
 
                     if (state == 0) {
-                        tick_count = 0; // Reset the tick counter to start counter for hold effect when the pedal will become 'on' in the next state change
+                        tick_count = 0; // Reset the tick counter to start counting for hold effect if the pedal will become 'on' in the next state change
                     }
                 } else {
                     // This is where experimentation comes into play
@@ -86,7 +87,7 @@ void main(void) {
         }
         
         if(GP1 == 1) {
-            __delay_ms(10); // debouncing
+            __delay_ms(10); // Debouncing
             if(GP1 == 1 && GP1 != button_state) {
                 button_state = GP1;
 
@@ -101,7 +102,6 @@ void main(void) {
         if (change_state == 1) {
             if (state == 0) { 
                 // change to on
-                
                 GP2 = 1; // photoFET on
                 __delay_ms(20);
                 GP0 = 1; // LED on
@@ -112,10 +112,11 @@ void main(void) {
                
                 // Write the state to EEPROM.
                 eeprom_write(0, 0xFF); // Write 0xFF at 0 address location
+                
+                // Set state to on
                 state = 1;
             } else { 
                 // change to off
-                
                 GP2 = 1; // photoFET on
                 __delay_ms(20);
                 GP0 = 0; // LED off
@@ -126,6 +127,8 @@ void main(void) {
                
                 // Write the state to EEPROM.
                 eeprom_write(0, 0x00); // Write 0x00 at 0 address location
+                
+                // Set state to off
                 state = 0;
             }
             change_state = 0; // reset change_state
